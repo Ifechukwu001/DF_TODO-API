bc6aa596feff2219b4345e49bca8c1e39953f3da
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2025-01-04 02:26:09 +0100

    Add initial migration and models for List and Action with UUID primary keys

--- src/lists/models.py
+++ src/lists/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from core.models import BaseModel
 
-# Create your models here.
+
+class List(BaseModel):
+    title = models.CharField(max_length=255)
+    note = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Action(BaseModel):
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name="actions")
+    description = models.CharField(max_length=100)
+    deadline = models.DateField(blank=True, null=True)
+    done = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.id.hex
