ad7e14f4010b8e483aea065981897e311d42606a
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2025-01-04 19:06:44 +0100

    Add deployment script for building and migrating application

---
+++ src/deployment/render_build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+pipx install uv
+uv sync
+uv pip install gunicorn uvicorn
+
+# Convert static asset files
+uv run src/manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+uv run src/manage.py migrate
