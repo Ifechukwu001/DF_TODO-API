b6042144aa3b7b8238c5dd151bdca338488e4d49
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2025-01-04 04:55:48 +0100

    Register ActionViewSet with list ID in the router and disable regex paths

--- src/lists/routers.py
+++ src/lists/routers.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import ListViewSet
+from .views import ListViewSet, ActionViewSet
 
 
-router = SimpleRouter()
+router = SimpleRouter(use_regex_path=False)
 
 
 router.register("lists", ListViewSet)
+router.register("lists/<uuid:list_id>/actions", ActionViewSet)
