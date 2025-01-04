e389ca7f6fb8158ca28db1359c2c9d92631d1675
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2025-01-04 02:26:45 +0100

    Refactor URL routing to include lists API and update schema view to use SpectacularJSONAPIView

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,18 +1,28 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import RedirectView
 from drf_spectacular.views import (
-    SpectacularAPIView,
+    SpectacularJSONAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
 
+api_paths = [
+    path("", include("lists.urls")),
+]
+
 urlpatterns = [
+    path(
+        "api/",
+        include(
+            api_paths,
+        ),
+    ),
     path("admin/", admin.site.urls),
     # Redirect to Swagger Docs
     path("", RedirectView.as_view(url="api/docs/")),
     # Swagger Docs
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/", SpectacularJSONAPIView.as_view(), name="schema"),
     path(
         "api/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
---
+++ src/lists/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+
+from .routers import router
+
+urlpatterns = [
+    path("v1/", include(router.urls)),
+]
