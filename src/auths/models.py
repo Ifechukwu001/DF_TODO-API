953204767df8902299b470725e0cde89a8bb6e8f
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2025-01-07 11:58:39 +0100

    Implement custom User model by extending AbstractUser and remove unnecessary fields

--- src/auths/models.py
+++ src/auths/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = None
+    first_name = None
+    last_name = None
+
+    REQUIRED_FIELDS = []
