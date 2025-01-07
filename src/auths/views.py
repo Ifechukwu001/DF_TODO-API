46180dd72a415b696469e6f8945b4094c8d61b74
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2025-01-07 11:59:22 +0100

    Add API views for user registration, login, and logout using dj-rest-auth

--- src/auths/views.py
+++ src/auths/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from dj_rest_auth.views import LoginView as DJLoginView, LogoutView as DJLogoutView
+from dj_rest_auth.registration.views import RegisterView as DJRegisterView
+from drf_spectacular.utils import extend_schema
 
-# Create your views here.
+
+class RegisterAPIView(DJRegisterView):
+    pass
+
+
+class LoginAPIView(DJLoginView):
+    pass
+
+
+class LogoutAPIView(DJLogoutView):
+    @extend_schema(exclude=True)
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(request=None, responses=None)
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
