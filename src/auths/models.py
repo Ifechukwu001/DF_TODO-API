cea8b22e79a365e16abf47a1d7afa66ff5f93eeb
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2025-01-22 04:48:44 +0100

    Remove unused authentication and list app files, including models, serializers, views, and URLs

--- src/auths/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    email = None
-    first_name = None
-    last_name = None
-
-    REQUIRED_FIELDS = []
--- src/lists/serializers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .list import ListSerializer  # noqa
-from .list import ListMutateSerializer  # noqa
-from .action import ActionSerializer  # noqa
-from .action import ActionMutateSerializer  # noqa
-from .action import ActionListSerializer  # noqa
